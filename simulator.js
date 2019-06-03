d4739c07a93bca41da022b49684469aac0fede06
micaelavdr <micaelavdr@gmail.com>
2019-06-03 15:20:39 +0100

    Add missing semicolons

--- simulator.js
+++ simulator.js
@@ -1,18 +1,22 @@
 module.exports.simulate = (input) => {
     const track = input.track;
     const travelLog = input.travelLog;
+    const lanes = ["a", "b", "c"];
     let lane = "b";
     let pos = 0;
     let obstacles = {};
 
     // map for easier search
     track.forEach(t => {
-        obstacles[t.position] = t.obstacles    
+        obstacles[t.position] = t.obstacles;    
     });
     
     for (var item in travelLog) { 
 
         item = travelLog[item];
+        if (parseInt(item.position) < 0) {
+            return { "status": "error" };        
+        }
 
         while (pos != item.position) {
             // handle reverse
@@ -25,25 +29,25 @@ module.exports.simulate = (input) => {
             // change lane
             if (pos == item.position) {
                 if (item.laneChange == "left") {
-                    lane = String.fromCharCode(lane.charCodeAt(0) - 1)
+                    lane = String.fromCharCode(lane.charCodeAt(0) - 1);
                 } else if (item.laneChange == "right") {
-                    lane = String.fromCharCode(lane.charCodeAt(0) + 1)
+                    lane = String.fromCharCode(lane.charCodeAt(0) + 1);
                 } else {
-                    return { "status": "error", "position": pos }
+                    return { "status": "error", "position": pos };
                 }
             }
             
             // check if out of bounds
-            if (lane < "a" || lane > "c") {
-                return { "status": "error", "position": pos }
+            if (!lanes.includes(lane)) {
+                return { "status": "error", "position": pos };
             }
 
             // check for obstacles
             if (obstacles[pos] && obstacles[pos].includes(lane)) {
-                return { "status": "error", "position": pos }
+                return { "status": "error", "position": pos };
             }
         }
-    };
+    }
 
-    return {"status": "success"}
+    return {"status": "success"};
 };
